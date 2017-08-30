977980cf62b65f003281c903c710d2d181721864
Herre Groen <herre@yoast.com>
2017-08-30 12:17:21 +0200

    Detect and handle ACF nested fields.

--- js/src/replacevars.js
+++ js/src/replacevars.js
@@ -6,6 +6,20 @@ var ReplaceVar = YoastReplaceVarPlugin.ReplaceVar;
 
 var supportedTypes = ['email', 'text', 'textarea', 'url', 'wysiwyg'];
 
+var createReplaceVar = function (field) {
+    // Remove HTML tags using jQuery in case of a wysiwyg field.
+    var content = (field.type === 'wysiwyg') ? jQuery( jQuery.parseHTML( field.content) ).text() : field.content;
+
+    var replaceVar = new ReplaceVar( '%%cf_'+field.name+'%%', content, { source: 'direct' } );
+    YoastSEO.wp.replaceVarsPlugin.addReplacement( replaceVar );
+
+    if (config.debug) {
+        console.log("Created ReplaceVar for: ", field.name, " with: ", content, replaceVars[field.name]);
+    }
+
+    return replaceVar;
+}
+
 var createReplaceVars = function (collect) {
     if (ReplaceVar === undefined) {
         if (config.debug) {
@@ -14,24 +28,17 @@ var createReplaceVars = function (collect) {
         return;
     }
 
-    fieldData   = _.filter(collect.getFieldData(), function (field) { return _.contains(supportedTypes, field.type) });
-    replaceVars = {}
+    var fieldData   = _.filter(collect.getFieldData(), function (field) { return _.contains(supportedTypes, field.type) });
+    var replaceVars = {}
 
     _.each(fieldData, function(field) {
-        // Remove HTML tags using jQuery in case of a wysiwyg field.
-        var content = (field.type === 'wysiwyg') ? jQuery( jQuery.parseHTML( field.content) ).text() : field.content;
-
-        replaceVars[field.name] = new ReplaceVar( '%%cf_'+field.name+'%%', content, { source: 'direct' } );
-        YoastSEO.wp.replaceVarsPlugin.addReplacement( replaceVars[field.name] );
-        if (config.debug) {
-            console.log("Created ReplaceVar for: ", field.name, " with: ", content, replaceVars[field.name]);
-        }
+        replaceVars[field.name] = createReplaceVar( field );
     });
 
     return replaceVars;
 };
 
-var updateReplaceVars = function (collect, replace_vars) {
+var updateReplaceVars = function (collect, replaceVars) {
     if (ReplaceVar === undefined) {
         if (config.debug) {
             console.log('Replacing ACF variables in the Snippet Window requires the latest version of wordpress-seo.');
@@ -39,10 +46,14 @@ var updateReplaceVars = function (collect, replace_vars) {
         return;
     }
 
-    fieldData = _.filter(collect.getFieldData(), function (field) { return _.contains(supportedTypes, field.type) });
+    var fieldData = _.filter(collect.getFieldData(), function (field) { return _.contains(supportedTypes, field.type) });
     _.each(fieldData, function(field) {
         // Remove HTML tags using jQuery in case of a wysiwyg field.
         var content = (field.type === 'wysiwyg') ? jQuery(jQuery.parseHTML(field.content)).text() : field.content;
+
+        if ( replaceVars[field.name] === undefined ) {
+            replaceVars[field.name] = createReplaceVar(field);
+        }
 
         replaceVars[field.name].replacement = content;
         if (config.debug) {
