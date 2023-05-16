6637129845c92fb7467352f33ff239ef0238413f
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-05-16 12:10:22 +0200

    Sets the minimum required Yoast SEO version to 20.8

--- inc/dependencies/dependency-yoast-seo.php
+++ inc/dependencies/dependency-yoast-seo.php
@@ -10,7 +10,7 @@
  */
 final class Yoast_ACF_Analysis_Dependency_Yoast_SEO implements Yoast_ACF_Analysis_Dependency {
 
-	const MINIMAL_REQUIRED_VERSION = 14.9;
+	const MINIMAL_REQUIRED_VERSION = '20.8';
 
 	/**
 	 * Checks if this dependency is met.
