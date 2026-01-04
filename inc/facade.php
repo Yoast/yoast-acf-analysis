3ab6eb938a93328b920a169ba5b6a67c1b601e29
jrfnl <jrfnl@users.noreply.github.com>
2026-01-04 01:38:46 +0100

    Modernize: use null coalesce equals operator
    ... as allowed and available since PHP 7.4.

--- inc/facade.php
+++ inc/facade.php
@@ -27,9 +27,7 @@ class Yoast_ACF_Analysis_Facade {
 	public static function get_registry() {
 		static $registry = null;
 
-		if ( $registry === null ) {
-			$registry = new Yoast_ACF_Analysis_Registry();
-		}
+		$registry ??= new Yoast_ACF_Analysis_Registry();
 
 		return $registry;
 	}
